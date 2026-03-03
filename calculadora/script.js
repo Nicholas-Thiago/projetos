e3ad3735fd84a9d12c80cdcb14c7e3c427c1bee4
Nicholas-Thiago <nicholasthiagosilva@gmail.com>
2026-03-03 20:22:38 -0300

    Transformei a interface do site em uma SPA e adicionei alguns visuais para saber em qual das seções o usuário está

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,3 +1,7 @@
+let sub = document.querySelector('.sub').classList.add('escondido')
+let mult = document.querySelector('.mult').classList.add('escondido')
+let divi = document.querySelector('.divi').classList.add('escondido')
+
 const adicao = (n1t, n2t) => {
     let resad = document.getElementById('resad')
     let n1 = Number(n1t.value)
@@ -23,4 +27,14 @@ const divisao = (n1t, n2t) => {
     let n1 = Number(n1t.value)
     let n2 = Number(n2t.value)
     resdiv.innerHTML = `<strong>${n1 / n2}</strong>`
+}
+
+const alternar = (classe) => {
+    adi = document.querySelector('.adi').classList.add('escondido')
+    sub = document.querySelector('.sub').classList.add('escondido')
+    mult = document.querySelector('.mult').classList.add('escondido')
+    divi = document.querySelector('.divi').classList.add('escondido')
+
+    let escolhido = document.querySelector('.' + classe)
+    escolhido.classList.remove('escondido')
 }
